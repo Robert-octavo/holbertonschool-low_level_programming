c819ddcab9cfcac899ea2d70d2f041a94ee91bb4
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-10 11:23:42 -0800

    Prints exactly a message with a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - prints exactly Programming is like building 
+ * a multilingual puzzle, followed by a new line.
+ * return: Always 0
+ *
+ */
 int main(void)
 {
 	char message[] = "\"Programming is like building a multilingual puzzle";
