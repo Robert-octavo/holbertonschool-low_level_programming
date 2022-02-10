404d546e1fab7d9ef9ec709406c9a5418013a19d
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-10 12:37:56 -0800

    Program that print witout using Prinf or Puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - program that prints exactly and that
+ * piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ *
+ * Return: Always 1.
+ *
+ */
+int main(void){
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",58);
+	return (1);
+}
