7bbb4a88ba8a5708a416e194300d20ec38f1cfa0
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-11 10:51:43 -0800

    Print the Alphabet in Lowercase and Uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,12 @@
  */
 int main(void)
 {
-	for (int x = 'A'; x <= 'Z'; x++)
-		putchar(tolower(x));
-	for (int x = 'A'; x <= 'Z'; x++)
-		putchar(x);
+	char c;
+
+	for (c = 'a'; c <= 'c'; c++)
+		putchar(c));
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
 	putchar('\n');
 
 	return (0);
