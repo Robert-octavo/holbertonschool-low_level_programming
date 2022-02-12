816b81913cf7ffc7ac1d28d7f14513c21baa312a
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-11 22:33:11 -0500

    Prints all possible combination from two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all possible
+ * combinations of singe-digit numbers
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if(i == 8 && j == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
