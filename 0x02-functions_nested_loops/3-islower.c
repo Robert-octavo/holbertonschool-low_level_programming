77477a5f992bed720a8935e80c05ce8a2b3e1c29
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-15 04:32:20 -0800

    checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - checks if a character is lowercase.
- *
+ * parameter: integer
  * Return: 1 if character is lowercase, 0 otherwise
- * 
+ *
  */
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
-		return(1);
+		return (1);
 	else
 		return (0);
 }
