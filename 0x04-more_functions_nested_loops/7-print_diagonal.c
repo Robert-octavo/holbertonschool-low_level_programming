77918494937e005032b0c39f9c55d7db8a670f25
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-17 02:32:54 -0500

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,19 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
 			if (i == j)
 			{
 				_putchar(92);
 			}
 			else if (j < i)
 				_putchar(32);
-			}
-			_putchar('\n');
 		}
-		
-	}
-	else
 		_putchar('\n');
+	}
 }
