0c2e60a5b15323487a6fae9ad98ad1a12b475072
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-17 02:47:44 -0500

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,18 @@ int main(void)
 {
 int i;
 
-for (i = 1; i < 101; i++)
+for (i = 1; i < 100; i++)
 {
 	if ((i % 3 == 0) && (i % 5 == 0))
 		printf("FizzBuzz ");
 	else if (i % 3 == 0)
 		printf("Fizz ");
 	else if (i % 5 == 0)
-	{
-		if(i == 100)
-			printf("Buzz");
-		else
-			printf("Buzz ");
-	}
+		printf("Buzz ");
 	else if(i % 3 != 0 && i % 5 != 0)
 		printf("%d ", i);
 }
+printf("Buzz");
 printf("\n");
 return (0);
 }
