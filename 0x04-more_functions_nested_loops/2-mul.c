28a3813ef26295c8819480d1ab3f86885224e436
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-17 01:05:37 -0500

    Multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * mul - function that checks for a
- * digit.
- * @a: integer
- * @b: integer
- * Return: result
- */
-void mul(int a, int b)
+* mul - function that checks for a
+* digit.
+* @a: integer
+* @b: integer
+* Return: result
+*/
+int mul(int a, int b)
 {
 	return (a * b);
 }
