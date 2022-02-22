c3db36c64c983aacc6fd8bafed1fccb9d0d3acd2
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-21 23:48:36 -0500

    copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,14 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (src[i])
 	{
 		dest[i] = src[i];
+		i++;
 	}
-	dest[i + 1] = '\0';
+
 	return (dest);
 
 }
