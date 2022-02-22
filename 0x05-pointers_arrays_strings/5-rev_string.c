28f29cd987378c44037305d49bd4242026c23e84
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-21 21:32:49 -0500

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rev_string - function that reverse a string
+* @s: pointer to char
+*/
+void rev_string(char *s)
+{
+	int lenght, i;
+	char s2;
+
+	lenght = 0;
+
+	while (s[lenght])
+	{
+		s2[lenght] = s[lenght]; 
+		lenght++;
+	}
+
+	lenght = lenght - 1;
+	i = 0;
+
+	while (lenght >= 0)
+	{
+		s[lenght] = s2[i];
+		lenght--;
+		i++;
+	}
+}
