d6745723fe4dc21ac5588cecca7570b3d50b68c2
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-25 09:20:45 -0500

    Encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,15 +10,15 @@
 char *leet(char *s)
 {
 	int i, j;
-	char l [] = "aAeEoOtTlL";
-	char n [] = "4433007711";
+	char l[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (s[i] == l[j])
-				s[i] = n[j];
+			if (*(s + i) == l[j])
+				*(s + i) = n[j];
 		}
 	}
 
