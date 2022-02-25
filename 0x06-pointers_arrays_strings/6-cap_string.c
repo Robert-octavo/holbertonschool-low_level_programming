3ce5cfb5229451a80380927ad0409e6f3ffa114c
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-25 08:34:18 -0500

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,29 +11,26 @@ char *cap_string(char *str)
 {
 	int i;
 
-	i = 0;
-
-	while(*(str + i) != '\0')
+	for (i = 0; *(str + i) != '\0'; i++)
 	{
 		while (!(*(str + i) >= 97 && *(str + i) <= 122))
 			i++;
 
-		if (*(str + i) - 1 == 32 || *(str + i) - 1 == 9 ||
-				*(str + i) - 1 == 10 ||
-				*(str + i) - 1 == 49 ||
-				*(str + i) - 1 == 59 ||
-				*(str + i) - 1 == 46 ||
-				*(str + i) - 1 == 21 ||
-				*(str + i) - 1 == 63 ||
-				*(str + i) - 1 == 34 ||
-				*(str + i) - 1 == 40 ||
-				*(str + i) - 1 == 41 ||
-				*(str + i) - 1 == 123 ||
-				*(str + i) - 1 == 125 ||
+		if (str[i - 1] == 32 || str[i - 1] == 9 ||
+				str[i - 1] == 10 ||
+				str[i - 1] == 49 ||
+				str[i - 1] == 59 ||
+				str[i - 1] == 46 ||
+				str[i - 1] == 21 ||
+				str[i - 1] == 63 ||
+				str[i - 1] == 34 ||
+				str[i - 1] == 40 ||
+				str[i - 1] == 41 ||
+				str[i - 1] == 123 ||
+				str[i - 1] == 125 ||
 				i == 0)
 			*(str + i) = *(str + i) - 32;
 
-		i++;
 	}
 
 	return (str);
