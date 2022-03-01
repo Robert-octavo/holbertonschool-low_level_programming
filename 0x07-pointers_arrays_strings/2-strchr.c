a474e0310afe5d07ff0e6519abf19fbf8b8dd8dd
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-02-28 22:09:19 -0500

    function that localtes a character in a str string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-
-	while (*(s + i))
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
-		return (s + i);
-		i++;
+			return (s + i);
 	}
 
 	if(*(src + i) == c)
