a5c0c3774eeb2621846fff3ce38ce63b6563484b
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-02 21:08:04 -0500

    Prints a string followed by a new liew line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function that prints a string,
+* followed by a new linw
+* @s: pointer to a char
+*
+* Return: Nothing
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_put_recursion((s + 1));
+	}
+}
