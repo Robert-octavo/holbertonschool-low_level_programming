9ac56995f2b9d03fb3339f58fa0b999ee3f72653
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-04 13:56:49 -0500

    Wirtes the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - printrs its name
+* @agrc: argument count
+* @argv: array of pointers
+*
+* Return: Nothing
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
