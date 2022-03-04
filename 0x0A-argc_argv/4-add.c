432eeb38a539505c47cd0aa92dc3df7885eb981b
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-04 15:39:33 -0500

    add two numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 	int i, j, suma;
 
 	suma = 0;
-	
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
