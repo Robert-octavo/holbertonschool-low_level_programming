7bbb472684c1f520f79a8f478c5566fc659904b6
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-04 14:34:09 -0500

    Wirtes the name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - printrs the name of the program
+* main - prints the name of the program
 * @agrc: argument count
 * @argv: array of pointers
 *
 * Return: Always 0
 */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	argc = argc;
 	printf("%s\n", *argv);
