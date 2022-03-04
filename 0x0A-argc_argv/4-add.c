4768a582e856f9196e25436421522987ebda38ac
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-04 15:28:06 -0500

    add two numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - a program that adds positive numbers
+* @argc: argument count
+* @argv: array of arguments
+* Return: Multiplication or 1
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, suma;
+
+	suma = 0;
+	
+	for(i = 1; i <= argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; i++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		suma = suma + atoi(argv[i]);
+	}
+
+	printf("%d\n", suma);
+	return (0);
+}
