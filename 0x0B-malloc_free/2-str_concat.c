a395efd65cb8178e30ada6a1a3709daca73fb641
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-03-08 20:59:09 -0500

    Concat 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(ptr + i) = *(s1 + i);
-	for (j = 0; *(s2 + j) != '\0'; i++)
+	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
 		*(ptr + i) = *(s2 + j);
 		i++;
