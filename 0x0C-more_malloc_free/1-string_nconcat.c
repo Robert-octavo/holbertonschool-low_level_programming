9e17d37c477b2304adbe27a7b234cd8cb1106cc4
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-11 11:47:22 -0500

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,6 +26,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	ptr = malloc(sizeof(char) * (lens1 + 1));
 
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(ptr + i) = *(s1 + i);
 
