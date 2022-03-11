3b1f6c197f05e7bf162a62c161ac966ca69c83ab
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-11 14:53:04 -0500

    allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (prt == null)
 		return (NULL);
 
-	for (i = 0; i < (size * nmemb) ; i++)
+	for (i = 0; i < (size * nmemb); i++)
 		*(ptr + i) = 0;
 
 	return (ptr);
