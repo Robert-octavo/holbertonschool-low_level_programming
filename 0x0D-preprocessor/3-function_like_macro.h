6a78b0147f302cc0574a148dd8ec5ff85a301470
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-14 14:07:07 -0500

    function like macro that computes the absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(X) ((x) < 0 ? ((x) * (-1)) : (x))
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
 #endif
