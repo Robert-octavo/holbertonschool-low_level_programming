1e82488cb2bc795bc46c90b1a042c26b4a55ff49
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-17 22:48:23 -0500

    Get Operator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,8 +1,11 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * get_op_func - Select the correct function
- * @s: The operator passed
- * Return: A Pointer
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -14,11 +17,12 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (i < 5)
 	{
-		if (*s == ops[i].op[0]
+		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
 		i++;
 	}
