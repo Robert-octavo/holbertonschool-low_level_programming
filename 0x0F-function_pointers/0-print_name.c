7c95c14cfd1acb037d52f7696a2cf48274af55e2
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-17 20:44:00 -0500

    Print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,9 @@
 #include "function_pointer.h"
+#include <stdio.h>
 /**
  * print_name - function that prints a name
  * @name: Pointer to char
- * @f: Pointer ro function
+ * @f: Pointer to function
  * Return: Nothing
  */
 void print_name(char *name, void(*f)(char *))
