712f9aafc4565c9905f11a947d3d71762b89c9c4
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-17 22:33:21 -0500

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,19 +1,15 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * int_index - write a function that searches for
- * an integer
- * @array: Pointer to array
- * @size: number of elements
- * @cmp: pointer to function
- * Return: first element or -1 if it fails
+ * main -performs an operation
+ * @argc: integer
+ * @argv: array
+ * Return: Result
  */
-int main(argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	int (*res)(int, int);
 	int num1, num2;
-	char *operador;
 
 	if (argc != 4)
 	{
@@ -21,17 +17,23 @@ int main(argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	operador = argv[2];
-
-	if (get_op_func(operador) == NULL || operador[1] != '\0')
+	if(strlen(argv[2]) > 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", get_op_func(operador)(num1, num2));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	res = get_op_func(argv[2]);
+
+	if (res == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", res(num1, num2));
 
 	return (0);
 }
