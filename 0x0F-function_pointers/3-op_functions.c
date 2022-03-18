648bce070b2e4501ebf8d1f6a959e419dd39c2ea
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-17 22:14:10 -0500

    op

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -10,6 +10,7 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
  * op_sub - substrac two numbers
  * @a: integer
@@ -20,6 +21,7 @@ int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
  * op_mul - multiply two numbers
  * @a: integer
@@ -30,6 +32,7 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
  * op_div - divides two numbers
  * @a: integer
@@ -41,11 +44,12 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		return (100);
+		exit(100);
 	}
 
 	return (a / b);
 }
+
 /**
  * op_mod - calculates the mod of two numbers
  * @a: integer
