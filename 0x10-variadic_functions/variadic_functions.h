8edd981fa437bbd0c67107ee231bd4514e9cbb1a
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-19 22:30:16 -0500

    updated file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+void print_c(va_list arg);
+void print_i(va_list arg);
+void print_f(va_list arg);
+void print_s(va_list arg);
+
+typedef struct f_print
+{
+    char *type;
+    void (*f)(va_list arg);
+} f_print_;
+
 #endif
