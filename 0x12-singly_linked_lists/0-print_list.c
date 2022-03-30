0bbff02f127e221714a329e722ab4d090727a6e7
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-29 20:21:16 -0500

    Add Readme file and Header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all
+ * the elements of a list_t list
+ * @h: singly linked list to print
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	while (h != NULL) 
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nill)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - function that finds the number of nodes
+ * in the list
+ * @h: singly linked list
+ * Return: number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	while (h != NULL) 
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
