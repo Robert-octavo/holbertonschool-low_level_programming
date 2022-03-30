da0246b860255752d6c6cb98f3f6270f1020e7f6
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-29 21:28:00 -0500

    update header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>	
+#include <stddef.h>
+#include <string.h>a
 
 typedef struct list_s
 {
@@ -13,6 +14,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
