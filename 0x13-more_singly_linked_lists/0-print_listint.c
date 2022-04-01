8fd480cf8ba731665629d450198f1ad029500b09
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-01 00:11:00 -0500

    update file - saltolinea

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
-	i = 0;
+	i = 0; /*Cantidad de nodes*/
 	while (h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
