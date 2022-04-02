8695c1e61454d47fb95fd6e0bc83315e2abcacbb
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-02 00:03:26 -0500

    updated file

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * sum_listint - function that deletes the head node of a listint_t
+ * sum_listint - function that deletes the head
+ * node of a listint_t
  * and returns the head node's data (n);
  * @head: head of the linked list.
  * Return: the sum of all data or 0 if it's NULL
