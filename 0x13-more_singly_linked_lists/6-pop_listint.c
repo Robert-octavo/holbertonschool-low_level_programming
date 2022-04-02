b6973ef32eee1f1a4dbdb679726e5ae899fa410c
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-01 23:50:05 -0500

    check file delete the head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * pop_listint - function that deletes the head node of a listint_t
+ * and returns the head node's data (n);
+ * @head: pointer to head of the linked list.
+ * Return: the head node's data or 0 if it's NULL
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *new;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+
+	new = malloc(sizeof(listint_t));/*Reserve Memory*/
+	if (new == NULL)/*Check if the Memory were allocated*/
+		return (NULL);
+
+	new = *head;
+	*head = (*head)->next;
+
+	return (data = (*head)->n);
+}
