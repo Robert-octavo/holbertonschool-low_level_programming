ab5b177bda876f213fa44d94c0deb3936ee0004f
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-02 00:00:09 -0500

    Sum the data of a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_listint - function that deletes the head node of a listint_t
+ * and returns the head node's data (n);
+ * @head: head of the linked list.
+ * Return: the sum of all data or 0 if it's NULL
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sumdata = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sumdata = sumadata + head->n;
+		head = head->next;
+	}
+
+	return (sumdata);
+}
