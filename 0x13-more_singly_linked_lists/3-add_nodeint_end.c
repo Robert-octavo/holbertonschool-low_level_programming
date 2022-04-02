0f2150f86e0a03b29941c0d261af585cb3366676
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-01 20:40:12 -0500

    Checking

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,17 +7,22 @@
  * Return: address of the head or Null if it failed.
  */
 
-listint_t *add_nodeint_end(list_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *new;
-	new = malloc(sizeof(list_t));/*Reserve Memory*/
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));/*Reserve Memory*/
 	if (new == NULL)/*Check if the Memory were allocated*/
 		return (NULL);
 
 	new->n = n;
-	while (head != NULL)
-	new->next = *head;
-	*head = new;
+	new->next = NULL;/*because it's gonna be the last one*/
+
+	if (*head == NULL)/*the new one it would be the first one*/
+	{
+		*head = new;
+		return (*head);
+	}
 
 	return (*head);
 }
