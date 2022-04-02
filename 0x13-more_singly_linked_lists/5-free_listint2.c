fedc9bd2adce4f43861fee6c4d86546a8a360ae1
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-01 23:21:52 -0500

    check if head it's Null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *c;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+	else
 	{
-		c = (*head)->next;
-		free(*head);
-		*head = c;
+		while (*head != NULL)
+		{
+			c = (*head)->next;
+			free(*head);
+			*head = c;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
