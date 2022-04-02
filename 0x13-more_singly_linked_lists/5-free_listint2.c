4819e74a2b7fdbc92c8edf8d19c5479965486de2
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-01 22:47:25 -0500

    Check the file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to the head of the linked list.
+ * Return: no return.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *c;
+
+	while (head != NULL)
+	{
+		c = head;
+		head = head->next;
+		free(c);
+	}
+}
