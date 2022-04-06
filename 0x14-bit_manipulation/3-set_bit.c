2430023f94af10576c11df7613c327a10d74e95a
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-05 23:45:31 -0500

    just the file

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_bit - Function that sets the value of
+ * a bit to 1 at a given index
+ * @n: Unsigned long int - number
+ * @index: Insigned long int - index
+ * Return: 1 if it worked or -1 if an error
+ */
+int set_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int value;
+	unsigned long int size = (sizeof(n) * 8);
+
+	if (index > size)
+		return (-1);
+
+	return (1);
+}
