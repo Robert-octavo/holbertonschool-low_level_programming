1b48a20aa565996c18ff1712126eb525825165b5
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-05 20:55:53 -0500

    add a condition

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -25,7 +25,9 @@ unsigned int binary_to_uint(const char *b)
 		if (b[len] != '0' && b[len] != '1')
                         return (0);
 
-		num = num + (b[len] - '0') * mul;
+		if (b[len] == '1')/*add if its find a 1*/
+			n = n + mul;
+
 		mul = mul * 2;
 	}
 	return (n);
