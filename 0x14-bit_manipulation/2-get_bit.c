797ea0ffbe7b60aec7a0bf0d62df38986ad9fe3a
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-05 23:41:00 -0500

    condition when index if > size

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,13 +5,15 @@
  * of a bit at a given index
  * @n: Unsigned long int - number
  * @index: Insigned long int - index
- * Return: the value of the bit at index or
- * -1 if an error occured
+ * Return: the value of the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int value;
 	unsigned long int size = (sizeof(n) * 8);
+
+	if (index > size)
+		return (-1);
 
 	if (n > 0)
 		value = (n >> index) & 1;
