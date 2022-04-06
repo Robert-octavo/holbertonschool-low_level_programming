bd486ee1b3c89c77b2a26b5e8910e2870a7d6c61
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-06 16:03:56 -0500

    first try

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - Function that reads a text file
+ * and prints it to the POSIX standar output
+ * @filename: Pointer
+ * @letters: number of letters it should read and print
+ * Return: The actual number of letters it could read and print
+ */
+ssize_t read_textfile(cons char *filename, size_t letters)
+{
+	int filed, vwrite, vread;
+	char *text;
+
+	text = malloc(sizeof(char) * letters);
+	
+	filed = open(filename, 0_RDONLY);/*Open the file with the flag Read Only*/
+	if (filed == -1)/*Check if return an error*/
+		return (0);
+	vread = read(filed, text, letters);
+	if (vread == -1)/*check if return an error*/
+		return (0);
+	vwrite = write(1, text, vread);
+	if (vwrite == -1)/*check if return an error*/
+		return (0);
+	free(text);
+
+
+	return (n);
+}
