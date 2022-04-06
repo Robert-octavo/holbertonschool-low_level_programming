a9dc2fb2f30f9dc1bbd787485f5b0841a15306d5
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-05 22:11:23 -0500

    Recursive using bitwise operator

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - Function that prints the binary
+ * representation of a number
+ * @n: Unsigned long int - number
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n != 0)
+		print_binary(n >> 1);/*right shift by 1 n*/
+	_putchar((n & 1) + '0');/*print the least significant bit*/
+}
