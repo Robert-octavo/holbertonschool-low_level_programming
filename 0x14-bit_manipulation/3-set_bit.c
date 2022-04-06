651164e9fd695f075cf9436012ea88ef723dc685
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-05 23:54:28 -0500

    change the prototype - pointer

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,13 +7,13 @@
  * @index: Insigned long int - index
  * Return: 1 if it worked or -1 if an error
  */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int value;
 	unsigned long int size = (sizeof(n) * 8);
 
 	if (index > size)
 		return (-1);
+	*n = *n | 1 << index;
 
 	return (1);
 }
