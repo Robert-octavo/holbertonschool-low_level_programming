2e43f08e7b5c0af0ad3b7b2908824c3aa22c7d66
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-05 22:15:07 -0500

    change the condition from n != 0 to n > 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n != 0)
+	if (n > 1)
 		print_binary(n >> 1);/*right shift by 1 n*/
 	_putchar((n & 1) + '0');/*print the least significant bit*/
 }
