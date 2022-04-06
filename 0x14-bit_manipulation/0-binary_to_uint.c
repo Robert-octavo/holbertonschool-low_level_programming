3f6b1ea65cf52bcf6f306d4f74b1c0cd171b8c91
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-05 20:35:04 -0500

    try with the condition inside of the second for

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,14 +18,12 @@ unsigned int binary_to_uint(const char *b)
 	if (b == '\0')
 		return (0);
 
-	for (len = 0; b[len] != '\0'; len ++)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-	}
+	for (len = 0; b[len] != '\0'; len ++);
 
 	for (len = len - 1; len >=0; len--)
 	{
+		if (b[len] != '0' && b[len] != '1')
+                        return (0);
 		num = num + (b[len] - '0') * mul;
 		mul = mul * 2;
 	}
