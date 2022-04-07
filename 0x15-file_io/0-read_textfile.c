f24d699ae53094e4edaad25d3e7cabb3004a16b8
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-06 22:20:15 -0500

    correct the parameters

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @letters: number of letters it should read and print
  * Return: The actual number of letters it could read and print
  */
-ssize_t read_textfile(cons char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int filed, vwrite, vread;
 	char *text;
