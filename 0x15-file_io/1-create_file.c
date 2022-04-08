f699fad31251f8d7a38366fd3c6c1e282105cfdb
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-07 21:26:20 -0500

    betty documentation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * create_file - Function that create a file
  * @filename: Pointer
- * @text_cointent: content
+ * @text_content: content
  * Return: 1 on success -1 on faiulure
  */
 int create_file(const char *filename, char *text_content)
