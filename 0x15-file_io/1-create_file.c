f8571c8d50d7021efae7f6d9bed14ba6d405fb4c
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-07 21:08:35 -0500

    Function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - Function that create a file
+ * @filename: Pointer
+ * @text_cointent: content
+ * Return: 1 on success -1 on faiulure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int openf, writef, largo = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	openf = open(filename, O_CREAT | O_WRONLY | N_TRUNC, 0600);
+	if (openf == -1)
+	{
+		close(openf);
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (*(text_content  + largo) != '\0')
+			largo++;
+		writef = write(openf, text_content, largo);
+		if (writef == -1)
+		{
+			write(STDOUT_FILENO, "fails", 6);
+			return (-1);
+		}
+
+	} 
+	close(openf);
+	return (1);
+}
