6c985f7b3e5c37183e862f48e098a9a36661b508
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-07 21:21:56 -0500

    change some conditions to write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		writef = write(openf, text_content, largo);
 		if (writef == -1)
 		{
-			write(STDOUT_FILENO, "fails", 6);
+			write(1, "fails", 5);
 			return (-1);
 		}
 
