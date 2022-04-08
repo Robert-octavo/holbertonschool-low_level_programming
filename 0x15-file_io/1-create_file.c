a4bc8a64d1227578ab84e44df1271b61d49b4a52
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-07 21:10:13 -0500

    correct some parameters

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
-	openf = open(filename, O_CREAT | O_WRONLY | N_TRUNC, 0600);
+	openf = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (openf == -1)
 	{
 		close(openf);
