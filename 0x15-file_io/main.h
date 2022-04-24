c6abafd2e6a363a76095eea74cc1c76f6c307d5e
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-04-24 18:18:21 -0500

    Update main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,8 +8,34 @@
 #include <unistd.h>
 
 int _putchar(char c);
+char *buffer(char *buffer);
+void c_file(int fd);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void c_file(int fd)
+{
+	int i;
+	i = close(fd);
+
+	if (i == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit (100);
+	}
+}
+
+char *buffer(char *buffer)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STERR_FILENO, "Error: Can't write to $s\n", buffer);
+		exit(99);
+	}
+	return (buf);
+}
 #endif
