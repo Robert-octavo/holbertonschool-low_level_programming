8ab6bc37d970449bbe70825319fb064fb83c7cbb
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-04-24 21:03:11 -0500

    Correct Betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < (index - 1); i++)
 	{
-		if(copia->next == NULL)
+		if (copia->next == NULL)
 			return (-1);
 		copia = copia->next;
 	}
