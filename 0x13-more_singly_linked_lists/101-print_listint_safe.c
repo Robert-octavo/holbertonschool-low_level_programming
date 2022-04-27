de4571d84a11a869816e07a1ba647b357b3c3050
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-04-26 20:51:33 -0500

    Add 101 file prints a list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint_safe - function that prints a listint_t
+ * linked list
+ * @head: pointer to a pointer.
+ * Return: The number of nodes in the list if it fails
+ * exit program with status 98
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *before = NULL;
+	listint_t *after = NULL;
+
+	
+	return (*head);
+}
