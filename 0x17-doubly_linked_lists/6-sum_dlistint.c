eaa944e8348a11109f4f600da01cb7a789973cf6
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-12 20:39:08 -0500

    Erase the conditional

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -10,14 +10,11 @@ int sum_dlistint(dlistint_t *head)
 {
 	int result = 0;
 
-	if (head == NULL)
-		return (0);
-
 	while (head != NULL)
 	{
 		result += head->n;
 		head = head->next;
 	}
-	
+
 return (result);
 }
