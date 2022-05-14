d9de000a7a9cacc622a9aafc9a28e3ad2a94248f
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-13 20:59:03 -0500

    change de condition

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,17 +24,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		index--;
 	}
 
-	if (tmp != *head)
-	{
-		(tmp->prev)->next = tmp->next;
-		if (tmp->next != NULL)
-			(tmp->next)->prev = tmp->prev;
-	}
-	else
+	if (tmp == *head)
 	{
 		*head = tmp->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
+	}
+	else
+	{
+		(tmp->prev)->next = tmp->next;
+		if (tmp->next != NULL)
+			(tmp->next)->prev = tmp->prev;
 	}
 	free(tmp);
 	return (1);
