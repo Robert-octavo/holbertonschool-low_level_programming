909dfcc7285ee416b67fce7b4d17925e5ff48d00
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-13 19:39:52 -0500

    Delete an specific point

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - Write a function that deletes
+ * the node at index index of a dlistint_t linked list.
+ * @head: head of the linked list.
+ * @idx: Index
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+/*check if the head is NULL*/
+	if (*head == NULL)
+		return (-1);
+
+/*Go to the index position with tmp*/
+	while (index != 1)
+	{
+		tmp = tmp->next;
+/*Check if tmp is NULL*/
+		if (tmp == NULL)
+			return (-1);
+		index--;
+	}
+
+	if (tmp != *head)
+	{
+		(tmp->prev)->next = tmp->next;
+		if(tmp->next != NULL)
+			(tmp->next)->prev = tmp->prev;
+	}
+	else
+	{
+		*head = tmp->next;
+	}
+	free(tmp);
+	return (1);
+}
