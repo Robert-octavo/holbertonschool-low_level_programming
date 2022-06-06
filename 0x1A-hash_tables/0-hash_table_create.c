781adeadabd6d3518a0625bdd9b55ca6fb5c712a
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-06 00:11:09 -0500

    Just the file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Write a function that creates a hash table.
+ * @size:  is the size of the array
+ * Return: a pointer to the newly created hash table
+ * If something went wrong, your function should return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
