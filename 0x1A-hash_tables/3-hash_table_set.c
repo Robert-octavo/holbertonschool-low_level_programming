60cc2f9fde50cea26103f1117baab068b4da6168
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-06 18:24:41 -0500

    just the file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Write a function that adds an element to the hash table.
+ * @ht: Write a function that adds an element to the hash table
+ * @value:  is the value associated with the key.
+ * @key: is the value associated with the key.
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	return (0);
+}
