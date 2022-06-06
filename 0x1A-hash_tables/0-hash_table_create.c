99cd5a6a2f7120888cb43262544d11229d2eadd2
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-06 00:24:57 -0500

    create a hast table - asign Malloc table and array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,5 +7,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *hash_t;
+	unsigned long int i;
 
+	hash_t = malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
+		return (NULL);
+
+	hash_t->size = size;
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_t->array == NULL)
+		return (NULL);
+
+	return (hash_t);
 }
