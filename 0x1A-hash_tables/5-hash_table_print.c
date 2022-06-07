5021d502b5586ee56f24e49258705e5544777a1f
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-06 20:47:26 -0500

    just the file

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Write a function that prints a hash table.
+ * @ht:   is the hash table
+ * Return: Nothing
+ * You should print the key/value in the order that they appear
+ * in the array of hash table
+ * Order: array, list
+ * If ht is NULL, don’t print anything
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	return (0);
+}
