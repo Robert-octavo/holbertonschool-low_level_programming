061894a173cf6c359ed7b4711185e7d7993b1202
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-06 22:59:34 -0500

    Just the file

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Write a function that deletes a hash table.
+ * @ht:  is the hash table
+ * Return: Nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	return (0);
+}
